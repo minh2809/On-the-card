f38a655b98862325bded9228637df1ac95829a45
minh2809 <nnmd28@gmail.com>
2022-04-06 01:08:34 +0700

    OTC

--- BTap/index.js
+++ BTap/index.js
@@ -4,26 +4,46 @@ const button = document.querySelector('.download-button');
 var width = document.querySelector('.container-wipe');
 var text = document.querySelector('.text');
 var text1 = document.querySelector('.text1');
-console.log(text)
-console.log(percent);
+var knob = document.querySelector('.spinner'); 
+var gradient = document.querySelector('.spinner-gradient');
 
 
- 
 var result = Math.floor(Math.random() * 6);
 
-
 function toggleDownload(){
-    setInterval(increasePercent,100);
+
+   //tăng số phần trăm
+    setInterval(increasePercent,100); 
     function increasePercent(){
-        if(result < 100 ){
+        if(result < 100 && 100 - result > 5){
         result = result + Math.floor(Math.random() * 6);
+        console.log(result)
         percent.innerHTML = result +'%';
+        }else if(result = 95){
+           result = result + 5;
+           percent.innerHTML = result +'%';
+        }else if (result = 96){
+         result = result + 4;
+         percent.innerHTML = result +'%';
+        }else if(result = 97){
+         result = result + 3;
+         percent.innerHTML = result +'%';
+        }else if (result = 98){
+         result = result + 2;
+         percent.innerHTML = result +'%';
+        }else if (result = 99){
+         result = result + 1;
+         percent.innerHTML = result +'%';
         }
      }
+
+     //tăng progressBar
      setInterval(increaseWidth,100);
      function increaseWidth(){
         width.style.width = `${result}%`;
      }
+
+     //thay đổi chữ khi chạy và khi hoàn thành
      setInterval(changeText,100);
      function changeText(){
         if(result < 100){
@@ -35,6 +55,25 @@ function toggleDownload(){
         }
      }
      
+     //xoay
+     setInterval(spinKnob,100);
+     function spinKnob(){
+        var degree = -90 + ((result/100)*360);
+        knob.style.transform = `rotate(${degree}deg)`;
+     }
+   
+     setInterval(removeGradient,34*100);
+     function removeGradient(){
+      if(result = 100){
+         gradient.style.opacity = 0;
+      }
+   }
 }
+
+
+
+
+
+
 
 button.addEventListener('click',toggleDownload);

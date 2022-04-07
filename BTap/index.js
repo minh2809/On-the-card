f5a39d5428e1f3e082e1dbd41a02b2a6c073e494
minh2809 <nnmd28@gmail.com>
2022-04-07 15:13:35 +0700

    OTC

--- BTap/index.js
+++ BTap/index.js
@@ -4,76 +4,89 @@ const button = document.querySelector('.download-button');
 var width = document.querySelector('.container-wipe');
 var text = document.querySelector('.text');
 var text1 = document.querySelector('.text1');
-var knob = document.querySelector('.spinner'); 
+var knob = document.querySelector('.knob'); 
 var gradient = document.querySelector('.spinner-gradient');
+var white1Arrow = document.querySelector('.white1-arrow');
+var whiteLine = document.querySelector('.white-line');
+var whiteArrow = document.querySelector('.white-arrow');
+var leftLine = document.querySelector('.line-left');
+var midLine = document.querySelector('.line-mid');
+var rightLine = document.querySelector('.line1-right');
+var left1Line = document.querySelector('.line1-left');
+var mid1Line = document.querySelector('.line1-mid');
+var right1Line = document.querySelector('.line1-right');
+var cursor = document.querySelector('.download-button')
+var spinner = document.querySelector('.spinner');
 
+console.log(cursor)
 
-var result = Math.floor(Math.random() * 6);
+var result = 0;
 
-function toggleDownload(){
+//Trong lúc Download
+   function increasePercent(){
+         if(result<100){
+            result = result + 4;
+            percent.innerHTML = result + "%";
+         }
+   }
 
-   //tăng số phần trăm
-    setInterval(increasePercent,100); 
-    function increasePercent(){
-        if(result < 100 && 100 - result > 5){
-        result = result + Math.floor(Math.random() * 6);
-        console.log(result)
-        percent.innerHTML = result +'%';
-        }else if(result = 95){
-           result = result + 5;
-           percent.innerHTML = result +'%';
-        }else if (result = 96){
-         result = result + 4;
-         percent.innerHTML = result +'%';
-        }else if(result = 97){
-         result = result + 3;
-         percent.innerHTML = result +'%';
-        }else if (result = 98){
-         result = result + 2;
-         percent.innerHTML = result +'%';
-        }else if (result = 99){
-         result = result + 1;
-         percent.innerHTML = result +'%';
-        }
-     }
+   function increaseWidth(){
+      width.style.width = `${result}%`;
+   }
 
-     //tăng progressBar
-     setInterval(increaseWidth,100);
-     function increaseWidth(){
-        width.style.width = `${result}%`;
-     }
-
-     //thay đổi chữ khi chạy và khi hoàn thành
-     setInterval(changeText,100);
-     function changeText(){
-        if(result < 100){
-           text.innerHTML = 'Downloading';
-           text1.innerHTML = 'Downloading';
-        }else{
-           text.innerHTML = 'Done';
-           text1.innerHTML = 'Done';
-        }
-     }
-     
-     //xoay
-     setInterval(spinKnob,100);
-     function spinKnob(){
-        var degree = -90 + ((result/100)*360);
-        knob.style.transform = `rotate(${degree}deg)`;
-     }
-   
-     setInterval(removeGradient,34*100);
-     function removeGradient(){
-      if(result = 100){
-         gradient.style.opacity = 0;
+   function changeText(){
+      if(result < 100){
+         text.innerHTML = "Downloading";
+         text1.innerHTML = "Downloading";
+      }else{
+         text.innerHTML = "Done";
+         text1.innerHTML = "Done";
       }
    }
-}
+
+   function spinKnob(){
+      var degree = -90 + (result / 100 ) * 360;
+      spinner.style.transform = `rotate(${degree}deg)`;
+   }
+
+   function onDownload(){
+      increasePercent();
+      increaseWidth();
+      changeText();
+      spinKnob();
+   }
+
+   function toggleDownload(){
+      setInterval(onDownload,700);
+      setInterval(afterDownload,25*705);
+   }
+
+   function toggleIcon(){
+      white1Arrow.style.top = '50px'
+      whiteLine.style.right = '-30px'
+      whiteArrow.style.top = '19px'
+   }
 
 
+//Sau khi Download
+   function deleteKnob(){
+      knob.style.opacity = 0;
+   }
 
+   function deleteGradient(){
+      gradient.style.opacity = 0;
+   }
 
+   function changeIcon(){
+      mid1Line.style.top = '40px';
+      right1Line.style.width = '22px';
+   }
 
-
+   function afterDownload(){
+      deleteKnob();
+      deleteGradient();
+      changeIcon();
+   }
 
 button.addEventListener('click',toggleDownload);
+button.addEventListener('click',toggleIcon);

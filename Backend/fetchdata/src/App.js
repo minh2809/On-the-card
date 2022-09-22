8a0c45df4d87df9a892d76fdd5da7fddb6c85a36
minh2809 <nnmd28@gmail.com>
2022-09-22 17:19:45 +0700

    OTC

---
+++ Backend/fetchdata/src/App.js
@@ -0,0 +1,12 @@
+import DataFetching from './components/DataFetching';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetching />
+    </div>
+  );
+}
+
+export default App;
